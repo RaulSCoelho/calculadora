23927e43e534f3dac4c0c6d2fe42894101937243
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-05-26 11:25:03 -0300

    'calculadora'

--- pages/index.js
+++ pages/index.js
@@ -6,81 +6,76 @@ function Home() {
             <h1>Calculadora</h1>
         </header>
         <section>
-            <section id='res'>
-                <div id='resultado'>
-
-                </div>
-            </section>
+            <input id="res" type="text" value="" readonly></input>
             <div id='teclado'>
                 <div>
                     <input id='btnum' type="button" value="7" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "7"
+                        var res = document.getElementById('res')
+                        res.value += "7"
                      }}></input><input id='btnum' type="button" value="8" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "8"
+                        var res = document.getElementById('res')
+                        res.value += "8"
                      }}></input><input id='btnum' type="button" value="9" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "9"
+                        var res = document.getElementById('res')
+                        res.value += "9"
                      }}></input><input id='mult' type="button" value="X" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        var resnumber = Number(res.value)
-                        res.innerHTML += "x"
+                        var res = document.getElementById('res')
+                        window.alert(res)
                      }}></input>
                 </div>
                 <div>
                     <input id='btnum' type="button" value="4" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "4"
+                        var res = document.getElementById('res')
+                        res.value += "4"
                      }}></input><input id='btnum' type="button" value="5" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "5"
+                        var res = document.getElementById('res')
+                        res.value += "5"
                      }}></input><input id='btnum' type="button" value="6" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "6"
+                        var res = document.getElementById('res')
+                        res.value += "6"
                      }}></input><input id='menos' type="button" value="-" onClick={() => {
-                        var res = document.getElementById('resultado')
+                        var res = document.getElementById('res')
                         var resnumber = Number(res.value)
-                        res.innerHTML += "-"
+                        res.value += "-"
                      }}></input>
                 </div>
                 <div>
                     <input id='btnum' type="button" value="1" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "1"
+                        var res = document.getElementById('res')
+                        res.value += "1"
                      }}></input><input id='btnum' type="button" value="2" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "2"
+                        var res = document.getElementById('res')
+                        res.value += "2"
                      }}></input><input id='btnum' type="button" value="3" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "3"
+                        var res = document.getElementById('res')
+                        res.value += "3"
                      }}></input><input id='mais' type="button" value="+" onClick={() => {
-                        var res = document.getElementById('resultado')
+                        var res = document.getElementById('res')
                         var resnumber = Number(res.value)
-                        res.innerHTML += "+"
+                        res.value += "+"
                      }}></input>
                 </div>
                 <div>
                     <input id='clear' type="button" value="C" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML = ""
+                        var res = document.getElementById('res')
+                        res.value = ""
                      }}></input><input id='btnum' type="button" value="0" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += "0"
+                        var res = document.getElementById('res')
+                        res.value += "0"
                      }}></input><input id='comma' type="button" value="," onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML += ","
+                        var res = document.getElementById('res')
+                        res.value += ","
                      }}></input><input id='div' type="button" value="÷" onClick={() => {
-                        var res = document.getElementById('resultado')
+                        var res = document.getElementById('res')
                         var resnumber = Number(res.value)
-                        res.innerHTML += "÷"
+                        res.value += "÷"
                      }}></input>
                 </div>
             </div>
             <div id='btn'>
                 <input id="igual" type="button" value="=" onClick={() => {
-                        var res = document.getElementById('resultado')
-                        res.innerHTML = 'Período de teste'
+                        var res = document.getElementById('res')
+                        res.value = 'Período de teste'
                      }}></input>
             </div>
         </section>
