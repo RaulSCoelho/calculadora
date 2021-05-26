df92b927785a6846735b69acd996ed46a3d59fa2
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-05-26 16:32:50 -0300

    'calculadora'

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 
 function Home() {
+    var cont = 0
+    var n2 = 1
     return (<div>
         <header>
             <h1>Calculadora</h1>
@@ -10,74 +12,146 @@ function Home() {
             <div id='teclado'>
                 <div>
                     <input id='btnum' type="button" value="7" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "7"
-                     }}></input><input id='btnum' type="button" value="8" onClick={() => {
+                        if(cont > 0){
+                            n2*= 7
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="8" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "8"
-                     }}></input><input id='btnum' type="button" value="9" onClick={() => {
+                        if(cont > 0){
+                            n2*= 8
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="9" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "9"
-                     }}></input><input id='mult' type="button" value="X" onClick={() => {
+                        if(cont > 0){
+                            n2*= 9
+                            cont--
+                        }
+                    }}></input><input id='mult' type="button" value="X" onClick={() => {
                         var res = document.getElementById('res')
                         var result = Number(res.value)
+                        n2 = result
                         res.value += 'x'
-                     }}></input>
+                        cont++
+                    }}></input>
                 </div>
                 <div>
                     <input id='btnum' type="button" value="4" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "4"
-                     }}></input><input id='btnum' type="button" value="5" onClick={() => {
+                        if(cont > 0){
+                            n2*= 4
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="5" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "5"
-                     }}></input><input id='btnum' type="button" value="6" onClick={() => {
+                        if(cont > 0){
+                            n2*= 5
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="6" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "6"
-                     }}></input><input id='menos' type="button" value="-" onClick={() => {
+                        if(cont > 0){
+                            n2*= 6
+                            cont--
+                        }
+                    }}></input><input id='menos' type="button" value="-" onClick={() => {
                         var res = document.getElementById('res')
                         var result = Number(res.value)
                         res.value += "-"
-                     }}></input>
+                    }}></input>
                 </div>
                 <div>
                     <input id='btnum' type="button" value="1" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "1"
-                     }}></input><input id='btnum' type="button" value="2" onClick={() => {
+                        if(cont > 0){
+                            n2*= 1
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="2" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "2"
-                     }}></input><input id='btnum' type="button" value="3" onClick={() => {
+                        if(cont > 0){
+                            n2*= 2
+                            cont--
+                        }
+                    }}></input><input id='btnum' type="button" value="3" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "3"
-                     }}></input><input id='mais' type="button" value="+" onClick={() => {
+                        if(cont > 0){
+                            n2*= 3
+                            cont--
+                        }
+                    }}></input><input id='mais' type="button" value="+" onClick={() => {
                         var res = document.getElementById('res')
                         var result = Number(res.value)
                         res.value += "+"
-                     }}></input>
+                    }}></input>
                 </div>
                 <div>
                     <input id='clear' type="button" value="C" onClick={() => {
                         var res = document.getElementById('res')
                         res.value = ""
-                     }}></input><input id='btnum' type="button" value="0" onClick={() => {
+                    }}></input><input id='btnum' type="button" value="0" onClick={() => {
+                        if(cont < 0){
+                            cont = 1
+                        }
                         var res = document.getElementById('res')
                         res.value += "0"
-                     }}></input><input id='comma' type="button" value="," onClick={() => {
+                        if(cont > 0){
+                            n2*= 0
+                            cont--
+                        }
+                    }}></input><input id='comma' type="button" value="," onClick={() => {
                         var res = document.getElementById('res')
                         res.value += ","
-                     }}></input><input id='div' type="button" value="÷" onClick={() => {
+                    }}></input><input id='div' type="button" value="÷" onClick={() => {
                         var res = document.getElementById('res')
                         var result = Number(res.value)
                         res.value += "÷"
-                     }}></input>
+                    }}></input>
                 </div>
             </div>
             <div id='btn'>
                 <input id="igual" type="button" value="=" onClick={() => {
-                        var res = document.getElementById('res')
-                        res.value = 'Período de teste'
-                     }}></input>
+                    var res = document.getElementById('res')
+                    res.value = n2
+                }}></input>
             </div>
         </section>
         <footer>
