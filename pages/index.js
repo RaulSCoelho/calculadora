0bfc6c205fd92355c46e12e30acff2b69be831a0
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-05-26 20:08:31 -0300

    'calculadora'

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 
 function Home() {
+    var resultado
+    var num9 = ""
+    var nove = 0
     var count = 0
     var n2 = 1
     return (<div>
@@ -17,12 +20,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 7
+                            resultado = n2 * 7
                             count--
-                        }else if(count > 2){
-                            n2 *= 7
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "7"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="8" onClick={() => {
                         var res = document.getElementById('res')
@@ -30,12 +37,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 8
+                            resultado = n2 * 8
                             count--
-                        }else if(count > 2){
-                            n2 *= 8
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "8"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="9" onClick={() => {
                         var res = document.getElementById('res')
@@ -43,24 +54,31 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 9
+                            resultado = n2 * 9
                             count--
-                        }else if(count > 2){
-                            n2 *= 9
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "9"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='mult' type="button" value="X" onClick={() => {
+                        n2 = resultado
+                        nove = 0
+                        num9 = ""
                         if (count == 0) {
                             var res = document.getElementById('res')
                             var result = Number(res.value)
                             n2 = result
                             res.value += 'x'
-                            count+= 2
-                        }else{
+                            count += 2
+                        } else {
                             res = document.getElementById('res')
                             res.value += 'x'
-                            count += 2
+                            count = 2
                         }
                     }}></input>
                 </div>
@@ -71,12 +89,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 4
+                            resultado = n2 * 4
                             count--
-                        }else if(count > 2){
-                            n2 *= 4
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "4"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="5" onClick={() => {
                         var res = document.getElementById('res')
@@ -84,12 +106,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 5
+                            resultado = n2 * 5
                             count--
-                        }else if(count > 2){
-                            n2 *= 5
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "5"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="6" onClick={() => {
                         var res = document.getElementById('res')
@@ -97,12 +123,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 6
+                            resultado = n2 * 6
                             count--
-                        }else if(count > 2){
-                            n2 *= 6
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "6"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='menos' type="button" value="-" onClick={() => {
                         var res = document.getElementById('res')
@@ -117,12 +147,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 1
+                            resultado = n2 * 1
                             count--
-                        }else if(count > 2){
-                            n2 *= 1
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "1"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="2" onClick={() => {
                         var res = document.getElementById('res')
@@ -130,12 +164,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 2
+                            resultado = n2 * 2
                             count--
-                        }else if(count > 2){
-                            n2 *= 2
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "2"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='btnum' type="button" value="3" onClick={() => {
                         var res = document.getElementById('res')
@@ -143,12 +181,16 @@ function Home() {
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 3
+                            resultado = n2 * 3
                             count--
-                        }else if(count > 2){
-                            n2 *= 3
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "3"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='mais' type="button" value="+" onClick={() => {
                         var res = document.getElementById('res')
@@ -162,18 +204,24 @@ function Home() {
                         res.value = ""
                         n2 = 1
                         count = 0
+                        num9 = ""
+                        resultado = 0
                     }}></input><input id='btnum' type="button" value="0" onClick={() => {
                         var res = document.getElementById('res')
                         res.value += "0"
                         if (count < 0) {
                             count = 1
                         }
+                        nove++
                         if (count == 2) {
-                            n2 *= 0
+                            resultado = n2 * 0
                             count--
-                        }else if(count > 2){
-                            n2 *= 0
-                            count -= 2
+                        } else if (count == 1) {
+                            for (nove; nove > 0; nove--) {
+                                num9 += "0"
+                            }
+                            var num9n = Number(num9)
+                            resultado = num9n * n2
                         }
                     }}></input><input id='comma' type="button" value="," onClick={() => {
                         var res = document.getElementById('res')
@@ -188,9 +236,10 @@ function Home() {
             <div id='btn'>
                 <input id="igual" type="button" value="=" onClick={() => {
                     var res = document.getElementById('res')
-                    res.value = n2
-                    count = 0          
-                    }}></input>
+                    res.value = resultado
+                    count = 0
+                    num9 = ""
+                }}></input>
             </div>
         </section>
         <footer>
