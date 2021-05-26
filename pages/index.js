f5f51a3cb8787bc89da5857a492c68b892eebf09
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-05-26 17:45:06 -0300

    'calculadora'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 function Home() {
-    var cont = 0
+    var count = 0
     var n2 = 1
     return (<div>
         <header>
@@ -12,73 +12,73 @@ function Home() {
             <div id='teclado'>
                 <div>
                     <input id='btnum' type="button" value="7" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "7"
-                        if(cont > 0){
-                            n2*= 7
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 7
                         }
                     }}></input><input id='btnum' type="button" value="8" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "8"
-                        if(cont > 0){
-                            n2*= 8
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 8
                         }
                     }}></input><input id='btnum' type="button" value="9" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "9"
-                        if(cont > 0){
-                            n2*= 9
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 9
                         }
                     }}></input><input id='mult' type="button" value="X" onClick={() => {
-                        var res = document.getElementById('res')
-                        var result = Number(res.value)
-                        n2 = result
-                        res.value += 'x'
-                        cont++
+                        if (count == 0) {
+                            var res = document.getElementById('res')
+                            var result = Number(res.value)
+                            n2 = result
+                            res.value += 'x'
+                            count++
+                        }else{
+                            res = document.getElementById('res')
+                            res.value += 'x'
+                            count++
+                        }
                     }}></input>
                 </div>
                 <div>
                     <input id='btnum' type="button" value="4" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "4"
-                        if(cont > 0){
-                            n2*= 4
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 4
                         }
                     }}></input><input id='btnum' type="button" value="5" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "5"
-                        if(cont > 0){
-                            n2*= 5
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 5
                         }
                     }}></input><input id='btnum' type="button" value="6" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "6"
-                        if(cont > 0){
-                            n2*= 6
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 6
                         }
                     }}></input><input id='menos' type="button" value="-" onClick={() => {
                         var res = document.getElementById('res')
@@ -88,34 +88,31 @@ function Home() {
                 </div>
                 <div>
                     <input id='btnum' type="button" value="1" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "1"
-                        if(cont > 0){
-                            n2*= 1
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 1
                         }
                     }}></input><input id='btnum' type="button" value="2" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "2"
-                        if(cont > 0){
-                            n2*= 2
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 2
                         }
                     }}></input><input id='btnum' type="button" value="3" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "3"
-                        if(cont > 0){
-                            n2*= 3
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 3
                         }
                     }}></input><input id='mais' type="button" value="+" onClick={() => {
                         var res = document.getElementById('res')
@@ -127,15 +124,16 @@ function Home() {
                     <input id='clear' type="button" value="C" onClick={() => {
                         var res = document.getElementById('res')
                         res.value = ""
+                        n2 = 1
+                        count = 0
                     }}></input><input id='btnum' type="button" value="0" onClick={() => {
-                        if(cont < 0){
-                            cont = 1
-                        }
                         var res = document.getElementById('res')
                         res.value += "0"
-                        if(cont > 0){
-                            n2*= 0
-                            cont--
+                        if (count < 0) {
+                            count = 1
+                        }
+                        if (count > 0) {
+                            n2 *= 0
                         }
                     }}></input><input id='comma' type="button" value="," onClick={() => {
                         var res = document.getElementById('res')
@@ -151,7 +149,8 @@ function Home() {
                 <input id="igual" type="button" value="=" onClick={() => {
                     var res = document.getElementById('res')
                     res.value = n2
-                }}></input>
+                    count = 0          
+                    }}></input>
             </div>
         </section>
         <footer>
